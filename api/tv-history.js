983cf1e3f1f8dbc80fca17eb39a88d5b9f490cbd
truongphuongtchq <truongphuong.tchq@gmail.com>
2026-03-23 07:49:14 +0700

    Update tv-history.js

--- api/tv-history.js
+++ api/tv-history.js
@@ -3,16 +3,16 @@ export default async function handler(req, res) {
 
   let base = 1250;
 
-  for (let i = 0; i < 120; i++) {
+  for (let i = 0; i < 100; i++) {
     const open = base;
     const move = (Math.random() - 0.5) * 10;
 
     const close = open + move;
-    const high = Math.max(open, close) + Math.random() * 3;
-    const low = Math.min(open, close) - Math.random() * 3;
+    const high = Math.max(open, close) + 3;
+    const low = Math.min(open, close) - 3;
 
     candles.push({
-      time: Math.floor(Date.now() / 1000) - (120 - i) * 60,
+      time: Math.floor(Date.now() / 1000) - (100 - i) * 60,
       open,
       high,
       low,
